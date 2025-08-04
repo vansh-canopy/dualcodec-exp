9923f2ce9f781966fb639303518dbf24ece73bd6
vansh <vansh@canopylabs.ai>
2025-08-04 10:44:12 +0000

    basic changes

--- dualcodec/infer/dualcodec/get_model.py
+++ dualcodec/infer/dualcodec/get_model.py
@@ -8,18 +8,22 @@ model_id_to_fname = {
     "25hz_v1": "dualcodec_25hz_16384_1024.safetensors",
     # use v2 for checkpoints
     "12hz_v2": "model.safetensors",
+    # use v3 for averaged model (needs custom names)
+    "12hz_v3": "",
 }
 model_id_to_cfgname = {
     "12hz_v1": "dualcodec_12hz_16384_4096_8vq.yaml",
     "25hz_v1": "dualcodec_25hz_16384_1024_12vq.yaml",
     # use v2 for checkpoints
     "12hz_v2": "dualcodec_12hz_16384_4096_8vq.yaml",
+    # use v3 for averaged model
+    "12hz_v3": "dualcodec_12hz_16384_4096_8vq.yaml",
 }
 
 from cached_path import cached_path
 
 
-def get_model(model_id="12hz_v1", pretrained_model_path="hf://amphion/dualcodec", strict=True):
+def get_model(model_id="12hz_v1", pretrained_model_path="hf://amphion/dualcodec", name=None, strict=True):
     import os
 
     # import importlib.resources as pkg_resources
